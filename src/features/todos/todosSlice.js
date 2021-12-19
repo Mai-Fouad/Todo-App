958ea6a7b66e18062d0394277233bc5acfbfb8ed
Mai-Fouad <maifouad136@gmail.com>
2021-12-19 11:25:33 +0200

    Todo App - react and redux

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -8,7 +8,7 @@ export default function todosReducer(state = initialState, action) {
             return action.payload;
         }
         case 'todos/todoAdded': {
-            return [ ...state, action.payload ];
+            return [...state, action.payload];
         }
         case 'todos/todoToggled': {
             return state.map((todo) => {
@@ -48,16 +48,30 @@ export default function todosReducer(state = initialState, action) {
             return state;
     }
 }
-
-export async function fetchTodos(dispatch, getState) {
-    const response = await client.get('/fakeApi/todos');
-    dispatch({ type: 'todos/todosLoaded', payload: response.todos });
+export const todosLoaded = todos => {
+    return {
+        type: 'todos/todosLoaded',
+        payload: todos
+    }
 }
+export const fetchTodos = () => async dispatch => {
+    const response = await client.get('/fakeApi/todos');
+    dispatch(todosLoaded(response.todos));
+}
+
+export const todoAdded = todo => ({type: 'todos/todoAdded',payload: todo});
 
 export function SaveNewTodo(text) {
     return async function saveNewTodoThunk(dispatch, getState) {
         const initialTodo = { text };
         const response = await client.post('/fakeApi/todos', { todo: initialTodo });
-        dispatch({ type: 'todos/todoAdded', payload: response.todo });
+        dispatch(todoAdded(response.todo));
+    }
+
+}
+export const colorFilterChanged = (color, changeType) => {
+    return {
+        type: 'filters/colorFilterChanged',
+        payload: { color, changeType }
     }
 }
